f1616841bc406af3b2905f18bf1e9945f10c16cb
Espoir SOSSOU <sossouespoir91@gmail.com>
2024-11-11 07:43:03 +0000

    BBI commit

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT || 4000);
-
+  const port = process.env.PORT || 3000; // Définit un port par défaut
+  await app.listen(port);
 
 }
 bootstrap();
